8fc110f28dae0101f1da7037415f93a72ddc4eee
v3ikko <patrickburns013@gmail.com>
2023-02-23 04:26:27 -0800

    ok

--- beatbox/functions.c
+++ beatbox/functions.c
@@ -6,6 +6,14 @@
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
+
+#include <alsa/asoundlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <limits.h>
+#include <alloca.h>
+
+
 #include <linux/i2c.h>
 #include "functions.h"
 
@@ -63,6 +71,14 @@ static unsigned char readI2cReg(int i2cFileDesc, unsigned char regAddr){
         exit(1);
     }
     return value;
+}
+
+void* playSound(void* args){
+    threadController* threadData = (threadController*) args;
+    while(threadData->programRunning){
+        sleep(0.2);
+    }
+    pthread_exit(0);
 }
 
 void* monitorAccelerometer(void* args){
@@ -181,6 +197,97 @@ void runCommand(char* command)
     }
 }
 
+// Open the PCM audio output device and configure it.
+// Returns a handle to the PCM device; needed for other actions.
+snd_pcm_t *Audio_openDevice()
+{
+	snd_pcm_t *handle;
+
+	// Open the PCM output
+	int err = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+	if (err < 0) {
+		printf("Play-back open error: %s\n", snd_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+
+	// Configure parameters of PCM output
+	err = snd_pcm_set_params(handle,
+			SND_PCM_FORMAT_S16_LE,
+			SND_PCM_ACCESS_RW_INTERLEAVED,
+			NUM_CHANNELS,
+			SAMPLE_RATE,
+			1,			// Allow software resampling
+			50000);		// 0.05 seconds per buffer
+	if (err < 0) {
+		printf("Play-back configuration error: %s\n", snd_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+
+	return handle;
+}
+
+// Read in the file to dynamically allocated memory.
+// !! Client code must free memory in wavedata_t !!
+void Audio_readWaveFileIntoMemory(char *fileName, wavedata_t *pWaveStruct)
+{
+	assert(pWaveStruct);
+
+	// Wave file has 44 bytes of header data. This code assumes file
+	// is correct format.
+	const int DATA_OFFSET_INTO_WAVE = 44;
+
+	// Open file
+	FILE *file = fopen(fileName, "r");
+	if (file == NULL) {
+		fprintf(stderr, "ERROR: Unable to open file %s.\n", fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	// Get file size
+	fseek(file, 0, SEEK_END);
+	int sizeInBytes = ftell(file) - DATA_OFFSET_INTO_WAVE;
+	fseek(file, DATA_OFFSET_INTO_WAVE, SEEK_SET);
+	pWaveStruct->numSamples = sizeInBytes / SAMPLE_SIZE;
+
+	// Allocate Space
+	pWaveStruct->pData = malloc(sizeInBytes);
+	if (pWaveStruct->pData == NULL) {
+		fprintf(stderr, "ERROR: Unable to allocate %d bytes for file %s.\n",
+				sizeInBytes, fileName);
+		exit(EXIT_FAILURE);
+	}
+
+	// Read data:
+	int samplesRead = fread(pWaveStruct->pData, SAMPLE_SIZE, pWaveStruct->numSamples, file);
+	if (samplesRead != pWaveStruct->numSamples) {
+		fprintf(stderr, "ERROR: Unable to read %d samples from file %s (read %d).\n",
+				pWaveStruct->numSamples, fileName, samplesRead);
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(file);
+}
+
+// Play the audio file (blocking)
+void Audio_playFile(snd_pcm_t *handle, wavedata_t *pWaveData)
+{
+	// If anything is waiting to be written to screen, can be delayed unless flushed.
+	fflush(stdout);
+
+	// Write data and play sound (blocking)
+	snd_pcm_sframes_t frames = snd_pcm_writei(handle, pWaveData->pData, pWaveData->numSamples);
+
+	// Check for errors
+	if (frames < 0)
+		frames = snd_pcm_recover(handle, frames, 0);
+	if (frames < 0) {
+		fprintf(stderr, "ERROR: Failed writing audio with snd_pcm_writei(): %li\n", frames);
+		exit(EXIT_FAILURE);
+	}
+	if (frames > 0 && frames < pWaveData->numSamples)
+		printf("Short write (expected %d, wrote %li)\n", pWaveData->numSamples, frames);
+}
+
 void startProgram(threadController* threadArgument){
     //Configure I2C Pins
     runCommand("config-pin p9_18 i2c");
@@ -207,6 +314,10 @@ void startProgram(threadController* threadArgument){
     pthread_create(&tid, &attr, printData, threadArgument);
     threadArgument->threadIDs[1] = tid;
 
+    //play sound thread
+    pthread_create(&tid, &attr, playSound, threadArgument);
+    threadArgument->threadIDs[2] = tid;
+
     //Wait for threads to gracefully return
     waitForProgramEnd(threadArgument);
 }
@@ -218,5 +329,7 @@ void waitForProgramEnd(threadController* threadArgument){
 
     //Wait for printing thread to join gracefully
     pthread_join(threadArgument->threadIDs[1],NULL);
-    
+
+    //Wait for printing thread to join gracefully
+    pthread_join(threadArgument->threadIDs[2],NULL);
 }
--- beatbox/functions.h
+++ beatbox/functions.h
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <alsa/asoundlib.h>
+
+// File used for play-back:
+// If cross-compiling, must have this file available, via this relative path,
+// on the target when the application is run. This example's Makefile copies the wave-files/
+// folder along with the executable to ensure both are present.
+#define SOURCE_FILE "wave-files/100060__menegass__gui-drum-splash-hard.wav"
+//#define SOURCE_FILE "wave-files/100053__menegass__gui-drum-cc.wav"
+
+#define SAMPLE_RATE   44100
+#define NUM_CHANNELS  1
+#define SAMPLE_SIZE   (sizeof(short)) 	// bytes per sample
+
+// Store data of a single wave file read into memory.
+// Space is dynamically allocated; must be freed correctly!
+typedef struct {
+	int numSamples;
+	short *pData;
+} wavedata_t;
+
+// Prototypes:
+snd_pcm_t *Audio_openDevice();
+void Audio_readWaveFileIntoMemory(char *fileName, wavedata_t *pWaveStruct);
+void Audio_playFile(snd_pcm_t *handle, wavedata_t *pWaveData);
+
 typedef struct threadController{
     //Hit on X
     int hitX;
@@ -19,6 +44,8 @@ typedef struct threadController{
 void startProgram(threadController* threadArgument);
 
 void waitForProgramEnd(threadController* threadArgument);
+
+void* playSound(void* args);
 
 void* monitorAccelerometer(void* args);
 
